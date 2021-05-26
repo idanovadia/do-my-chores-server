20b856d4aa6f135b8f539ed6ea41673bcc5cdefa
idan <idanov@post.bgu.ac.il>
2021-05-26 19:55:38 +0300

    Initial commit

---
+++ src/main/java/com/domychoresserver/domychoresserver/DoMyChoresServerApplication.java
@@ -0,0 +1,13 @@
+package com.domychoresserver.domychoresserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoMyChoresServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoMyChoresServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/domychoresserver/domychoresserver/DoMyChoresServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domychoresserver.domychoresserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoMyChoresServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
